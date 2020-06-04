fd22ccc4081300ec89f74234634124bd4bad52eb
wilfried.augeard@etu.u-bordeaux.fr <wilfried.augeard@etu.u-bordeaux.fr>
2020-06-04 15:06:18 +0200

    Implémentation des choiceBox de la page Ajouter

--- src/sample/Controller/AjouterController.java
+++ src/sample/Controller/AjouterController.java
@@ -1,9 +1,13 @@
 package sample.Controller;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import sample.Model.ICategory;
 
 import java.io.IOException;
 import java.net.URL;
@@ -14,11 +18,32 @@ public class AjouterController implements Initializable {
 
     @FXML
     private AnchorPane rootPane;
+    @FXML
+    private TextField revenusTextField;
+    @FXML
+    private ChoiceBox revenusChoiceBox;
+    @FXML
+    private TextField depensesTextField;
+    @FXML
+    private ChoiceBox depensesChoiceBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         controller = Controller.getController();
+        display();
     }
+
+    public void display(){
+        for(ICategory category : controller.getRevenuesCategory().getChildren()){
+            revenusChoiceBox.getItems().add(category.getName());
+        }
+        revenusChoiceBox.getSelectionModel().selectFirst();
+        for(ICategory category : controller.getDepensesCategory().getChildren()){
+            depensesChoiceBox.getItems().add(category.getName());
+        }
+        depensesChoiceBox.getSelectionModel().selectFirst();
+    }
+
 
     @FXML
     public void backWindow() throws IOException {
